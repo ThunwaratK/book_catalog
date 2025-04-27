f8138c8cb7dc9e457fe257faf9b7c6591e0da4ef
unknown <thunwarat.1812@gmail.com>
2025-04-27 20:10:20 +0700

    v0.4

--- config/database.js
+++
@@ -1,9 +0,0 @@
-const cassandra = require('cassandra-driver');
-
-const client = new cassandra.Client({
-  contactPoints: ['cassandra:9042'],
-  localDataCenter: 'datacenter1',
-  keyspace: 'book_catalog'
-});
-
-module.exports = client;
